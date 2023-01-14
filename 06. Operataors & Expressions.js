6a6684b3412ef36889a9076a7b8c79e9368944a3
Dhruv Chaurasia <dhruvchaurasia52@gmail.com>
2023-01-14 21:30:55 +0530

    Initial Commit

---
+++ 06. Operataors & Expressions.js
@@ -0,0 +1,46 @@
+// // Arithmetic Operators
+
+let a = 15;
+let b = 5;
+
+console.log("a + b = ", a + b);
+console.log("a - b = ", a - b);
+console.log("a / b = ", a / b);
+console.log("a * b = ", a * b);
+console.log("a ** b = ", a ** b);
+console.log("a++ = ", a++);
+console.log("++a = ", ++a);
+console.log("b-- = ", b--);
+console.log("--b = ", --b);
+
+// // Assignment Operator
+
+// let assignment = 1;
+// assignment += 5
+// console.log(assignment)
+
+// Comparison operators
+
+// let a = 5
+// let b = 5
+
+// console.log("a == b", a == b)
+// console.log("a != b", a != b)
+// console.log("a === b", a === b)
+// console.log("a !== b", a !== b)
+
+// // Difference Between == and ===
+
+// let aa = 5
+// let bb = "5"
+
+// console.log("aa == bb", aa == bb)
+// console.log("aa != bb", aa != bb)
+// console.log("aa === bb", aa === bb)
+// console.log("aa !== bb", aa !== bb)
+
+// Logical Operators
+
+// let x = 5;
+// let y = 3;
+// console.log(x < y && x == 5)
---
+++ 07. Conditional Expressions.js
@@ -0,0 +1,10 @@
+let a = prompt("Hey What's your age?");
+a = Number.parsenInt(a); // Converting the string to a number
+
+if (a < 0) {
+  alert("You are not eligible to apply for DL");
+} else if (a < 9) {
+  alert("You are a kid");
+} else {
+  alert("You are not eligible to apply for DL");
+}
