a2d17db0e92a54a222dd2d9abe80c8142adf80ff
Dhruv Chaurasia <dhruvchaurasia52@gmail.com>
2023-01-16 19:42:42 +0530

    Initial commit

---
+++ 13. Strings.js
@@ -0,0 +1,16 @@
+// String
+let name = "Arun Kumar"
+console.log(name)
+let frined = "John"
+console.log(name[0])
+console.log(frined.length)
+
+// Template Literal
+let friend1 = "Mohit"
+let friend2 = "Nikhil"
+let sentence = `${friend1} is a close friend of ${friend2}`
+console.log(sentence)
+
+// Escape Sequence characters
+let fruit = 'Banana\nApple'
+console.log(fruit)
---
+++ 14. String Methods.js
@@ -0,0 +1,6 @@
+let name = "ROHIT"
+console.log(name.length)
+console.log(name.toUpperCase())
+console.log(name.toLowerCase())
+console.log(name.slice(2,4))
+console.log(name.replace("IT", "AN"))
