cedbf4996d1c5694e05eec3a11cfbae1a536204e
Dhruv Chaurasia <dhruvchaurasia52@gmail.com>
2023-03-04 00:05:29 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,44 @@
+// document.writre ('hello world')
+// alert('Hello world');
+// console.log('Hello world');
+
+// const name = "john";
+// const lastname = 'Snakeandbake';
+// let fullName;
+// console.log('Hello there, your full name is : ' + name + lastname);
+
+// const website = "google";
+// const url = "http://www." + website + ".com";
+// console.log(url);
+
+// const number = 34;
+// let pants = 2.567;
+// pants = "are great";
+
+// console.log(number);
+// console.log(pants);
+
+// const random = 56 + 67 + 6 - 3 * 2;
+// const random2 = (56 + 8 - 3) / 5;
+
+// console.log(random);
+// console.log(random2);
+
+// Array
+
+// const f1 = "anna";
+// const f2 = "anna";
+// const f3 = "anna";
+
+// const friends = ["john", "peter", "Bob", "susy", 78, undefined, null];
+
+// console.log(friends[1]);
+// console.log(friends[2]);
+// console.log(friends[4]);
+
+function hello() {
+  // logic
+  console.log("Hello there Hob");
+  console.log("Hello there Nob");
+  console.log("Hello there susy");
+}
